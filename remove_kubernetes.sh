b227b95afaef28d1ffcd27315f6f524fee4e34d9
Sergio Giménez <hi@sergiogimenez.com>
2023-09-28 14:12:06 +0000

    Avoid yes/no prompt

--- remove_kubernetes.sh
+++ remove_kubernetes.sh
@@ -4,6 +4,6 @@ set -x
 
 # Remove kubernetes
 kubeadm reset
-sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*
-sudo apt-get autoremove
+sudo apt-get -y purge kubeadm kubectl kubelet kubernetes-cni kube*
+sudo apt-get -y autoremove
 sudo rm -rf ~/.kube
